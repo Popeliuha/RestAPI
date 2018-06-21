3c6bf3adc2b79f5f933874f696d26a8c2e51dcbe
Surikenchik <popelyshkon25@gmail.com>
2018-06-21 18:19:49 +0300

    Initial commit. Two simple Get and Post methods.

---
+++ GetApiFromPetShop/PetStoreClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace GetApiFromPetShop
+{
+    public class PetStoreClient
+    {
+        RestClient client = new RestClient("https://petstore.swagger.io/v2");
+
+        public string GetByOrderId(int orderId)
+        {
+            if (orderId <= 10 && orderId >= 1)
+            {
+                var request = new RestRequest($"/store/order/{orderId}", Method.GET);
+                IRestResponse response = client.Execute(request);
+
+                return response.Content;
+            }
+            else
+            {
+                return "Id is invalid.";
+            }
+        }
+
+        public string PostPet()
+        {
+            var request = new RestRequest("/pet", Method.POST);
+            request.AddHeader("content-type", "application/json");
+            string json = @"{
+                  ""id"": 13,
+                  ""category"": {
+                                ""id"": 13,
+                    ""name"": ""kotik""
+                  },
+                  ""name"": ""Kot"",
+                  ""photoUrls"": [
+                    ""lallaa""
+                  ],
+                  ""tags"": [
+                    {
+                      ""id"": 13,
+                      ""name"": ""hehehehe""
+                    }
+                  ],
+                  ""status"": ""unavailable""
+                }";
+            request.AddParameter("text/json", json, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            return response.Content;
+        }
+    }
+}
---
+++ GetApiFromPetShop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetApiFromPetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new PetStoreClient();
+            var getValidByOrderId = client.GetByOrderId(2);
+            Console.WriteLine(getValidByOrderId);
+            var getInvalidByOrderId = client.GetByOrderId(12);//id should be 1 to 10
+            Console.WriteLine(getInvalidByOrderId);//order will be not found
+            var postPet = client.PostPet();
+            Console.WriteLine(postPet);
+            Console.Read();
+        }
+    }
+}
+//прочитати про патерни, апі
